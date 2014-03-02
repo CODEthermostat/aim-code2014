d14aff8dc23d39c60d6eb16afdac681f30b023a7
Blonde <mpbarcante1@gmail.com>
2014-03-02 11:03:13 -0500

    Front-end

---
+++ app/views/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+
+<html lang="en">
+<html>
+        <head>
+                <title>AIM</title>
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <link type="text/css" href = "css/style.css" rel = "stylesheet">
+                <link type="text/css" href="css/bootstrap-theme.css">
+                <link type="text/css" href="css/bootstrap.css">
+                <link type="text/css" href="css/flat-ui.css">
+
+                <link href='http://fonts.googleapis.com/css?family=Muli' rel='stylesheet' type='text/css'>
+        </head>
+        <body style="background-image:url(images/front.jpg); background-size:cover;">
+ 
+               <div id="front">
+
+                    <a href="place.html" style="text-decoration:none"><h3 style="font-family: 'Muli', sans-serif; color:#FFFFFF; font-size:110px; text-align:center;">AIM</h3></a>
+                    <p style="font-family: 'Muli', sans-serif; color:#FFFFFF; font-size:12px; text-align:center; margin-top:-130px">MARKET RESEARCH REDEFINED</p>
+
+               </div>
+
+               
+        </body>
+</html>
---
+++ app/views/price.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html>
+        <head>
+                <title>Bootstrap 3</title>
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <link href = "css/bootstrap.min.css" rel = "stylesheet">
+                <link href = "css/style.css" rel = "stylesheet">
+
+
+                <link href='http://fonts.googleapis.com/css?family=Roboto:100' rel='stylesheet' type='text/css'>
+                <link href='http://fonts.googleapis.com/css?family=Muli' rel='stylesheet' type='text/css'>
+                <link href='http://fonts.googleapis.com/css?family=Josefin+Sans' rel='stylesheet' type='text/css'>
+        </head>
+        <body>
+ 
+                <div class = "navbar navbar-default navbar-static-top">
+                        <div class = "container">
+                               
+                                <a href = "#" class = "navbar-brand" style="font-family:'Century Gothic', sans-serif; font-color:#0000">AIM</a>
+                               
+                                <button class = "navbar-toggle" data-toggle = "collapse" data-target = ".navHeaderCollapse">
+                                        <span class = "icon-bar"></span>
+                                        <span class = "icon-bar"></span>
+                                        <span class = "icon-bar"></span>
+                                </button>
+                               
+                                <div class = "collapse navbar-collapse navHeaderCollapse">
+                               
+                                         <ul class = "nav navbar-nav navbar-right" style="font-family:'Josefin Sans', sans-serif; font-size:12px;">
+                                       
+                                                <li class = "active" style="text-align:center;"><a href = "#">HOME</a></li>
+                                                <li style="text-align:center; "><a href = "#">ABOUT</a></li>
+                                                <li style="text-align:center;"><a href = "#">CONTACT</a></li>
+                                       
+                                        </ul>
+                               
+                                </div>
+                               
+                        </div>
+                </div>
+
+
+
+                <div class="location">
+                    <img src="images/price.png">
+                    <h3>PRICE<h3>
+
+                </div>
+
+                <div class="bottomicons">
+
+                    <ul>
+
+                    <li><a href="place.html"><img src="images/location.png"</a></li>
+                    <li><a href="people.html"><img src="images/people.png"</a></li>
+                    <li><a href="product.html"><img src="images/product.png"</a></li>
+                    <li><a href="price.html"><img src="images/price.png"</a></li>
+                    <li><a href="advertising.html"><img src="images/marketing.png"</a></li>
+
+                    </ul>    
+
+                </div>
+
+ 
+                <script src = "http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+                <script src = "js/bootstrap.js"></script>
+               
+        </body>
+
+
+
+</html>
